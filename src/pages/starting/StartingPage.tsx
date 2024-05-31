3a4f53ea1a5ddb018e077a3a65dbb261f8bda05f
Ildar <ildaryxa@gmail.com>
2024-06-01 01:49:10 +0300

    feat: add new title in starting page

--- src/pages/starting/StartingPage.tsx
+++ src/pages/starting/StartingPage.tsx
@@ -2,7 +2,13 @@ import { FC, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import { CenteredPage } from '../../layouts';
-import { Button, Dropdown, Panel, Tooltip } from '../../shared/components';
+import {
+  Button,
+  Dropdown,
+  Panel,
+  Title,
+  Tooltip,
+} from '../../shared/components';
 
 const ContextMenu = styled(Dropdown)`
   background: blue;
@@ -19,6 +25,7 @@ const StartingPage: FC = () => {
   return (
     <CenteredPage>
       <Panel width={500} height={500}>
+        <Title>Hello world</Title>
         <Tooltip title="Откроется модальное окно для заполнения данных">
           <Button ref={buttonRef} onClick={() => setIsOpen((prev) => !prev)}>
             Click me
