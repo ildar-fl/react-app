9ae2b41f0b2d6069b122b764f93794222d4ee002
Ildar <ildaryxa@gmail.com>
2024-06-01 14:23:40 +0300

    feat: create deploy

--- src/pages/starting/StartingPage.tsx
+++ src/pages/starting/StartingPage.tsx
@@ -2,7 +2,13 @@ import { FC, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import { CenteredPage } from '../../layouts';
-import { Button, Dropdown, Panel, Tooltip } from '../../shared/components';
+import {
+  Button,
+  Dropdown,
+  Panel,
+  Title,
+  Tooltip,
+} from '../../shared/components';
 
 const ContextMenu = styled(Dropdown)`
   background: blue;
@@ -17,6 +23,7 @@ const StartingPage: FC = () => {
   return (
     <CenteredPage>
       <Panel width={500} height={500}>
+        <Title>Стартовая страница</Title>
         <Tooltip title="Откроется модальное окно для заполнения данных">
           <Button ref={buttonRef} onClick={() => setIsOpen((prev) => !prev)}>
             Click me
